36bef4e9c0dd27e3e6666807120954fa0f23ebcc
Eliú Mendoza <124463994+edu-kalel@users.noreply.github.com>
2026-04-27 11:13:35 -0600

    Add: valid-tree - submission-6

---
+++ Data Structures & Algorithms/valid-tree/submission-6.java
@@ -0,0 +1,39 @@
+class Solution {
+
+    Set<Integer> visited;
+    Map<Integer, List<Integer>> hmap;
+    public boolean validTree(int n, int[][] edges) {
+
+        // adjacency list
+        hmap = new HashMap<>();
+        // initialize
+        for(int i = 0 ; i<n ; i++){
+            hmap.put(i, new ArrayList<>());
+        }
+        // fill
+        for(int[] edge : edges){
+            hmap.get(edge[0]).add(edge[1]);
+            hmap.get(edge[1]).add(edge[0]);
+        }
+        // adjacency list complete
+        // now dfs
+        visited = new HashSet<>();
+        return dfs(-1, 0) && visited.size()==n;
+    }
+
+    boolean dfs(int prev, int current){
+        if(visited.contains(current)){
+            return false;
+        }
+        visited.add(current);
+        for(int i : hmap.get(current)){
+            if(i==prev){
+                continue;
+            }
+            if(!dfs(current, i)){
+                return false;
+            }
+        }
+        return true;
+    }
+}
