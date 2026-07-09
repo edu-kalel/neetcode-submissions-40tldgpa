4b84964a01530fbc616f07048c82d684f1e6bdcc
Eliú Mendoza <124463994+edu-kalel@users.noreply.github.com>
2026-07-09 13:38:40 -0600

    Add: is-anagram - submission-8

---
+++ Data Structures & Algorithms/is-anagram/submission-8.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s.length()!=t.length()){
+            return false;
+        }
+        Map<Character, Integer> maps = new HashMap<>();
+        Map<Character, Integer> mapt = new HashMap<>();
+        for(int i = 0 ; i<s.length() ; i++){
+            maps.put(s.charAt(i), maps.getOrDefault(s.charAt(i), 0)+1);
+            mapt.put(t.charAt(i), mapt.getOrDefault(t.charAt(i), 0)+1);
+        }
+        return maps.equals(mapt);
+    }
+}
